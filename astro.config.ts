2503870b60ed0e0cc57fefb0edc8c9871ea4d95b
Christian Fuss <christian@hackyourshack.org>
2023-12-06 12:11:48 +0000

    Added PWA

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,33 @@
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
+import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
+import manifest from "./webmanifest.json";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-pwa-recipe.vercel.app/",
   base: "/",
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  integrations: [mdx()],
+  adapter: vercel(),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  integrations: [
+    mdx(),
+    AstroPWA({
+      mode: "production",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+      manifest,
+    }),
+  ],
 });
