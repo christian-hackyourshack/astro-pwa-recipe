733dba5f5c64a1a9ce319418ffa4bffa6e613171
Christian Fuss <christian@hackyourshack.org>
2023-12-12 07:04:41 +0000

    Introduced more complex structureto reflect real world scanrios better

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import mdx from "@astrojs/mdx";
-// import node from "@astrojs/node";
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
+// import vercel from "@astrojs/vercel/serverless";
 import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
 import type { ManifestOptions } from "vite-plugin-pwa";
@@ -10,11 +10,11 @@ import manifest from "./webmanifest.json";
 export default defineConfig({
   site: "https://astro-pwa-recipe.vercel.app/",
   base: "/",
-  output: "server",
-  adapter: vercel(),
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
+  output: "hybrid",
+  // adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [
     mdx(),
     AstroPWA({
@@ -23,6 +23,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.svg"],
       workbox: {
+        navigateFallback: "/default.offline",
         globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
       },
     }),
