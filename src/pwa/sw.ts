e44bdb2bde9bb082f30a30299f2ef3eb0c8a7a6e
Christian Fuss <christian@hackyourshack.org>
2023-12-12 19:26:48 +0000

    Added hierarchical offline pages

--- src/pwa/sw.ts
+++ src/pwa/sw.ts
@@ -1,9 +1,15 @@
 /// <reference lib="webworker" />
 declare let self: ServiceWorkerGlobalScope;
 
-import { clientsClaim } from "workbox-core";
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { cacheNames, clientsClaim } from "workbox-core";
+import {
+  cleanupOutdatedCaches,
+  getCacheKeyForURL,
+  precacheAndRoute,
+} from "workbox-precaching";
 import { imageCache, pageCache, staticResourceCache } from "workbox-recipes";
+import { setCatchHandler, setDefaultHandler } from "workbox-routing";
+import { NetworkOnly } from "workbox-strategies";
 
 cleanupOutdatedCaches();
 
@@ -15,6 +21,31 @@ precacheAndRoute(self.__WB_MANIFEST);
 pageCache({ warmCache: ["/", "/catalog/"] });
 staticResourceCache();
 imageCache();
+
+setCatchHandler(async (options) => {
+  const { destination, url } = options.request;
+
+  if (destination === "document") {
+    const cache = await caches.open(cacheNames.precache);
+    const path = url.split("/");
+
+    while (path.length >= 3) {
+      const offlineUrl = path.join("/") + "/offline/index.html";
+      const cacheKey = getCacheKeyForURL(offlineUrl);
+      console.log({ path, offlineUrl, cacheKey });
+      if (cacheKey) {
+        const offlinePage = await cache.match(cacheKey);
+        if (offlinePage) {
+          return offlinePage;
+        }
+      }
+      path.pop();
+    }
+  }
+  return Response.error();
+});
+
+setDefaultHandler(new NetworkOnly());
 
 // Forcefully update all clients immediately also in other browser tabs
 self.skipWaiting();
